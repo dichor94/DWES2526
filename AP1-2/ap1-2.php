022d0d3ab5921701f06c8b0fb629bc3360b9c72a
Diego Chaparro <dichor@alumnatflorida.es>
2025-09-14 14:15:21 +0200

    Pongo el array dentro de la condición que indica si es por GET.

--- AP1-2/ap1-2.php
+++ AP1-2/ap1-2.php
@@ -1,15 +1,13 @@
 <?php
 
-$data = [];
-
-
-
 
 
 //Pongo dos variables diferentes para hacer el ejercicio, lo ideal es usar una sesión para que no se borre nada cuando lo estemos usando.
 
 if($_SERVER["REQUEST_METHOD"] == "GET"){
 
+
+    $data = []; //Array para meter las claves con su valor
     $key1 = $_GET["clave1"];
     $value1 = $_GET["valor1"];
     $key2 = $_GET["clave2"];
