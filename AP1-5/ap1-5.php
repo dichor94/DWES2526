99a510fd279e819daeca2a15b7f2d5f7491277b6
Diego Chaparro <dichor@alumnatflorida.es>
2025-09-15 22:50:05 +0200

    Añado la primera versión del código ejercicio 5

--- AP1-5/ap1-5.php
+++ AP1-5/ap1-5.php
@@ -4,14 +4,39 @@
 $host = "servidor_db";
 $username = "root";
 $password = "root";
-$database = "todolist";
+$database = "AP1";
 
-$mysqli = new mysqli($host, $username, $password);
+$pdoConnect = new PDO("mysql:host=$host;dbname=$database", $username, $password);
 
-if ($mysqli->connect_error) {
+try{
 
-    die("Error de conexión: " . $mysqli->connect_error);
+    $pdoConnect = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    print "<strong>Conexión realizada correctamente a la base de datos $database</strong> <br><br>";
+
+    //Querys
+
+    //SELECT
+    $stmtSelect = $pdoConnect->prepare("SELECT id, nombre, estado FROM usuarios");
+    $stmtSelect->execute();
+    $arraySelect = $stmtSelect->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+}catch (PDOException $error){
+
+    print "Error de conexión!:" . $error->getMessage();
 
 }
 
-echo "Conexión exitosa";
+
+    if(count($arraySelect) > 0 and is_array($arraySelect)){
+
+        foreach($arraySelect as $value){
+
+            print "El usuario con nombre " . $value["nombre"] . " posee la id " . $value["id"] . " y su estado es " . $value["estado"] . "<br>";
+
+
+        }
+
+    }
